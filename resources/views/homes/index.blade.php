c412d0fdbefbde1d87c2fb8f7d57d3843879b6d5
liyang <liyangcoder@163.com>
2021-10-16 22:48:39 +0800

    修改logo

--- resources/views/homes/index.blade.php
+++ resources/views/homes/index.blade.php
@@ -59,45 +59,45 @@
 
             @includeWhen($isOpenSeckill, 'homes.seckills')
 
-            <section class="section latest-coupons-area ptb-30">
-                <header class="panel ptb-15 prl-20 pos-r mb-30">
-                    <h3 class="section-title font-18">优惠券</h3>
-                    <a href="/coupon_templates" class="btn btn-o btn-xs pos-a right-10 pos-tb-center">查看所有</a>
-                </header>
+{{--            <section class="section latest-coupons-area ptb-30">--}}
+{{--                <header class="panel ptb-15 prl-20 pos-r mb-30">--}}
+{{--                    <h3 class="section-title font-18">优惠券</h3>--}}
+{{--                    <a href="/coupon_templates" class="btn btn-o btn-xs pos-a right-10 pos-tb-center">查看所有</a>--}}
+{{--                </header>--}}
 
-                <div class="row row-masnory row-tb-20">
+{{--                <div class="row row-masnory row-tb-20">--}}
 
-                    @foreach($couponTemplates as $template)
-                        <div class="col-sm-6 col-lg-4">
+{{--                    @foreach($couponTemplates as $template)--}}
+{{--                        <div class="col-sm-6 col-lg-4">--}}
 
-                        <div class="coupon-item">
-                            <div class="style-three">
-                                <div class="info-box">
-                                    <p class="nick">{{ $template->title }}</p>
-                                    <div class="coupon-money">
-                                        <div class="lay of">￥<em>{{ $template->amount }}</em></div>
-                                        <div class="lay">
-                                            @if ($template->full_amount > 0)
-                                                <p class="tit">满{{ $template->full_amount }}元可用</p>
-                                            @else
-                                                <p class="tit">无门槛</p>
-                                            @endif
-                                            <p class="demand"
-                                               style="color: #FFB800;">{{ $template->start_date }} ~</p>
-                                            <p class="demand"
-                                               style="color: #5FB878;">{{ $template->end_date }}</p>
-                                        </div>
-                                    </div>
-                                </div>
-                                <a href="/coupon_templates" class="get-btn">
-                                    <span>去领取</span>
-                                </a>
-                            </div>
-                        </div>
-                        </div>
-                    @endforeach
-                </div>
-            </section>
+{{--                        <div class="coupon-item">--}}
+{{--                            <div class="style-three">--}}
+{{--                                <div class="info-box">--}}
+{{--                                    <p class="nick">{{ $template->title }}</p>--}}
+{{--                                    <div class="coupon-money">--}}
+{{--                                        <div class="lay of">￥<em>{{ $template->amount }}</em></div>--}}
+{{--                                        <div class="lay">--}}
+{{--                                            @if ($template->full_amount > 0)--}}
+{{--                                                <p class="tit">满{{ $template->full_amount }}元可用</p>--}}
+{{--                                            @else--}}
+{{--                                                <p class="tit">无门槛</p>--}}
+{{--                                            @endif--}}
+{{--                                            <p class="demand"--}}
+{{--                                               style="color: #FFB800;">{{ $template->start_date }} ~</p>--}}
+{{--                                            <p class="demand"--}}
+{{--                                               style="color: #5FB878;">{{ $template->end_date }}</p>--}}
+{{--                                        </div>--}}
+{{--                                    </div>--}}
+{{--                                </div>--}}
+{{--                                <a href="/coupon_templates" class="get-btn">--}}
+{{--                                    <span>去领取</span>--}}
+{{--                                </a>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                        </div>--}}
+{{--                    @endforeach--}}
+{{--                </div>--}}
+{{--            </section>--}}
 
 
             <section class="section latest-deals-area ptb-30">
@@ -145,60 +145,60 @@
                 </div>
             </section>
 
-            <section class="section stores-area stores-area-v1 ptb-30">
-                <header class="panel ptb-15 prl-20 pos-r mb-30">
-                    <h3 class="section-title font-18">活跃的用户</h3>
-                    <a href="#" class="btn btn-o btn-xs pos-a right-10 pos-tb-center">-</a>
-                </header>
-                <div class="popular-stores-slider owl-slider" data-loop="true" data-autoplay="true" data-smart-speed="1000" data-autoplay-timeout="10000" data-margin="20" data-items="2" data-xxs-items="2" data-xs-items="2" data-sm-items="3" data-md-items="5" data-lg-items="6">
-                    @foreach ($users as $user)
-                        <div class="store-item t-center">
-                            <a href="#" class="panel is-block">
-                                <div class="embed-responsive embed-responsive-4by3">
-                                    <div class="store-logo">
-                                        <img class="user-avatar" src="{{ $user->avatar }}" alt="{{ $user->HiddenName }}">
-                                    </div>
-                                </div>
-                                <h6 class="store-name ptb-10">{{ $user->HiddenName }}</h6>
-                            </a>
-                        </div>
-                    @endforeach
-                </div>
-            </section>
+{{--            <section class="section stores-area stores-area-v1 ptb-30">--}}
+{{--                <header class="panel ptb-15 prl-20 pos-r mb-30">--}}
+{{--                    <h3 class="section-title font-18">活跃的用户</h3>--}}
+{{--                    <a href="#" class="btn btn-o btn-xs pos-a right-10 pos-tb-center">-</a>--}}
+{{--                </header>--}}
+{{--                <div class="popular-stores-slider owl-slider" data-loop="true" data-autoplay="true" data-smart-speed="1000" data-autoplay-timeout="10000" data-margin="20" data-items="2" data-xxs-items="2" data-xs-items="2" data-sm-items="3" data-md-items="5" data-lg-items="6">--}}
+{{--                    @foreach ($users as $user)--}}
+{{--                        <div class="store-item t-center">--}}
+{{--                            <a href="#" class="panel is-block">--}}
+{{--                                <div class="embed-responsive embed-responsive-4by3">--}}
+{{--                                    <div class="store-logo">--}}
+{{--                                        <img class="user-avatar" src="{{ $user->avatar }}" alt="{{ $user->HiddenName }}">--}}
+{{--                                    </div>--}}
+{{--                                </div>--}}
+{{--                                <h6 class="store-name ptb-10">{{ $user->HiddenName }}</h6>--}}
+{{--                            </a>--}}
+{{--                        </div>--}}
+{{--                    @endforeach--}}
+{{--                </div>--}}
+{{--            </section>--}}
 
-            <section class="section subscribe-area ptb-40 t-center">
-                <div class="newsletter-form">
-                    <h4 class="mb-20"><i class="fa fa-envelope-o color-green mr-10"></i>订阅我们</h4>
-                    <p class="mb-20 color-mid">每周六上午八点将发送一封商品推荐信息给你 <br />(测试阶段将为每天发送一封订阅邮件)</p>
+{{--            <section class="section subscribe-area ptb-40 t-center">--}}
+{{--                <div class="newsletter-form">--}}
+{{--                    <h4 class="mb-20"><i class="fa fa-envelope-o color-green mr-10"></i>订阅我们</h4>--}}
+{{--                    <p class="mb-20 color-mid">每周六上午八点将发送一封商品推荐信息给你 <br />(测试阶段将为每天发送一封订阅邮件)</p>--}}
 
-                        <div class="input-group mb-10">
+{{--                        <div class="input-group mb-10">--}}
 
-                            @if ($loginUser)
-                                <input  type="email" id="subscribe_email" class="form-control bg-white"
-                                        value="{{ $loginUser->subscribe->email ?? optional($loginUser)->email }}"
-                                        placeholder="请输入邮箱地址"
-                                        {{ $loginUser->subscribe ? 'disabled' : ''  }}
-                                required="required">
-                                <span class="input-group-btn">
-                                    <button class="btn {{ $loginUser->subscribe ? 'btn-warning' : '' }}" id="subscribe" type="button">
-                                        {{ $loginUser->subscribe ? '取消订阅' : '订阅' }}
-                                    </button>
-                                 </span>
-                            @else
-                                <input  type="email" id="subscribe_email" class="form-control bg-white"
-                                        value=""
-                                        placeholder="请输入邮箱地址"
-                                        required="required">
-                                <span class="input-group-btn">
-                                    <button class="btn" id="login_subscribe_btn" type="button" style="">订阅</button>
-                                 </span>
-                            @endif
+{{--                            @if ($loginUser)--}}
+{{--                                <input  type="email" id="subscribe_email" class="form-control bg-white"--}}
+{{--                                        value="{{ $loginUser->subscribe->email ?? optional($loginUser)->email }}"--}}
+{{--                                        placeholder="请输入邮箱地址"--}}
+{{--                                        {{ $loginUser->subscribe ? 'disabled' : ''  }}--}}
+{{--                                required="required">--}}
+{{--                                <span class="input-group-btn">--}}
+{{--                                    <button class="btn {{ $loginUser->subscribe ? 'btn-warning' : '' }}" id="subscribe" type="button">--}}
+{{--                                        {{ $loginUser->subscribe ? '取消订阅' : '订阅' }}--}}
+{{--                                    </button>--}}
+{{--                                 </span>--}}
+{{--                            @else--}}
+{{--                                <input  type="email" id="subscribe_email" class="form-control bg-white"--}}
+{{--                                        value=""--}}
+{{--                                        placeholder="请输入邮箱地址"--}}
+{{--                                        required="required">--}}
+{{--                                <span class="input-group-btn">--}}
+{{--                                    <button class="btn" id="login_subscribe_btn" type="button" style="">订阅</button>--}}
+{{--                                 </span>--}}
+{{--                            @endif--}}
 
-                        </div>
+{{--                        </div>--}}
 
-                    <p class="color-muted"><small>我们永远不会与第三方分享您的电子邮件地址.</small> </p>
-                </div>
-            </section>
+{{--                    <p class="color-muted"><small>我们永远不会与第三方分享您的电子邮件地址.</small> </p>--}}
+{{--                </div>--}}
+{{--            </section>--}}
         </div>
     </div>
 
